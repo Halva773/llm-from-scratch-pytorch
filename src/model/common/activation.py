3df05d35ff7c71baf538b3f8215239063a9357c3
Halva773 <Selin0106@mail.ru>
2026-02-08 14:46:51 +0300

    Add SwiGLU activation function implementation

--- src/model/common/activation.py
+++ src/model/common/activation.py
@@ -15,3 +15,20 @@ class SiLU(nn.Module):
     
     def forward(self, x: torch.Tensor):
         return x*torch.sigmoid(x)
+    
+
+class SwiGLU(nn.Module):
+    def __init__(self, emb_size: int, dropout: float = 0.1, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.gate = nn.Linear(emb_size, 4 * emb_size)
+        self.up = nn.Linear(emb_size, 4 * emb_size)
+        self.down = nn.Linear(4 * emb_size, emb_size)
+        self.silu = SiLU()
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: torch.Tensor):
+        gate = self.silu(self.gate(x))
+        up = self.up(x)
+        down = self.down(gate * up)
+        out = self.dropout(down)
+        return out
